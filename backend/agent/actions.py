0c779cb7e41ccd099c5e173cd2e5216799aa13e5
Bruce Moe <brucemoe1@gmail.com>
2024-10-20 14:31:46 -0700

    Changes

--- backend/agent/actions.py
+++ backend/agent/actions.py
@@ -7,6 +7,8 @@ bedrock_runtime = boto3.client(
     region_name="us-east-1",
 )
 
+roles = ["duelist", "sentinel", "controller", "initiator"]
+
 # Call Titan model
 def call_titan(prompt):
     prompt_config = {
@@ -35,8 +37,28 @@ def call_titan(prompt):
     results = response_body.get("results")[0].get("outputText")
     return results
 
+def build_team(team_condition, player_data):
+    """
+    Returns a 5-player Valorant roster
+    """
+    context = ""
+    for role in roles:
+        context += get_top_players_by_role(team_condition, role, player_data)
 
-def get_top_players_by_role(team_setting, role, player_data):
+    prompt = f"""Use the following pieces of context to answer the question at the end.
+
+    {player_data}
+
+    Question: {f"Build a 5-man team with the top players in each role for {team_condition}. 
+    Ensure that the team composition is balanced in terms of roles and that it has exactly one in-game leader(IGL). "}""
+    Answer:"""
+
+    generated_text = call_titan(prompt)
+    print(generated_text)
+
+    return generated_text
+
+def get_top_players_by_role(team_condition, role, player_data):
     """
     Returns the top 3 players in a role for the given team setting using the player data.
     """
@@ -44,14 +66,14 @@ def get_top_players_by_role(team_setting, role, player_data):
 
     {player_data}
 
-    Question: {f"Find the top 3 {role} players that play in {team_setting}."}"
+    Question: {f"Find the top 3 {role} players that play in {team_condition}."}"
     Answer:"""
 
     generated_text = call_titan(prompt)
     print(generated_text)
 
     resp_string = (
-        f"\n The top 3 players that play {role} in {team_setting} are: \n" +
+        f"\n The top 3 players that play {role} in {team_condition} are: \n" +
         generated_text
     )
 
